6a4a778318917e1cb28b816299eaf2908d9806c3
Rajský Matyáš <149485684+rrrajsky@users.noreply.github.com>
2024-11-26 10:07:40 +0100

    Delete python directory

--- python/pocasi/main.py
+++
@@ -1,6 +0,0 @@
-isRaining = False
-isWindy = False
-isFoggy = False
-
-if not isFoggy and not isRaining and not isWindy:
-    print("the weather is nice, don't take anything")
--- python/vojaci/main.py
+++
@@ -1,5 +0,0 @@
-soldier = 1
-
-while soldier <= 300:
-    print(str(soldier) + ". soldier at Thermopyl greets their king.")
-    soldier += 1
